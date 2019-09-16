3182ecd55c4c61e0409837fcc4aeeae4cc7b2468
CUB3D <callumthom11@gmail.com>
2019-09-16 05:45:46 +0100

    Implemented upload and status of buckets

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,72 @@
 use actix::*;
 
-use actix_web_actors::{ws, HttpContext};
-use actix_web::{HttpResponse, web, HttpRequest, HttpServer, App, Error as AWError, middleware};
-use actix_web_actors::ws::Message;
-use serde::Deserialize;
-use futures::future::{Future, ok};
-use std::net::SocketAddr;
+use actix_web::{HttpResponse, web, HttpServer, App, Error as AWError, middleware};
+use serde::{Deserialize, Serialize};
+use futures::future::Future;
 use futures::stream::Stream;
-use serde_json;
 use std::path::Path;
 use std::fs::{File, create_dir};
 use std::io::{Read, Write};
-use std::borrow::Borrow;
+use walkdir::WalkDir;
+use sha1::Sha1;
 
 extern crate futures;
-#[macro_use]
-extern crate debug_rs;
+
+//fn walk_bucket(name: String) -> Vec<&'static Path> {
+//    let mut paths = Vec::new();
+//
+//    for e in WalkDir::new(format!("storage_root/{}/", name)).into_iter().filter_map(|e| e.ok()) {
+//        if e.metadata().unwrap().is_file() {
+//            println!("{}", e.path().display());
+//            paths.push(e.path().clone())
+//        }
+//    }
+//
+//    return paths;
+//}
+
+#[derive(Deserialize, Serialize)]
+struct Blob {
+    blob_name: String,
+    blob_sha1: String,
+}
+
+#[derive(Serialize)]
+struct Bucket {
+    blobs: Vec<Blob>
+}
+
+fn bucket_verify(
+    file: web::Path<BucketLocation>
+) -> Result<HttpResponse, AWError> {
+    let mut blobs = Vec::new();
+
+    for e in WalkDir::new(format!("storage_root/{}/", &file.name)).into_iter().filter_map(|e| e.ok()) {
+        if e.metadata().unwrap().is_file() {
+            println!("{}", e.path().display());
+            let path = e.path();
+
+            let mut sha = Sha1::new();
+
+            let mut file = File::open(path).unwrap();
+            let mut content = String::new();
+            file.read_to_string(&mut content);
+
+            sha.update(content.as_bytes());
+
+            let hex_string = sha.digest().to_string();
+
+            blobs.push(Blob {
+                blob_name: path.to_str().unwrap().to_string(),
+                blob_sha1: hex_string
+            })
+        }
+    }
+
+    Ok(HttpResponse::Ok().json(Bucket {
+        blobs
+    }))
+}
 
 #[derive(Deserialize)]
 struct BucketLocation {
@@ -35,26 +86,31 @@ fn bucket_create(
 fn bucket_upload(
     file: web::Path<FileLocation>,
     data: web::Payload
-) -> Result<HttpResponse, AWError> {
-
+) -> impl Future<Item = HttpResponse, Error = AWError> {
     let path_str = format!("storage_root/{}/{}", &file.bucket_name, &file.file_name);
     let file = File::create(path_str);
-    if let Ok(mut file) = file {
+//    if let Ok(mut file) = file {
+//        data.concat2().then(|bytes| {
+//            web::block(move || {
+//                if let Ok(bytes) = bytes {
+//                    file.write_all(&bytes).expect("Unable to save file")
+//                }
+//            }).from_err()
+//        })
+//    } else {
+//        HttpResponse::BadRequest().finish()
+//    }
 
-        Arbiter::spawn_fn({
-                              data.concat2().then(|bytes| {
-                                  web::block(move || {
-                                      if let Ok(bytes) = bytes {
-                                          file.write_all(&bytes);
-                                      }
-
-                                      Ok(())
-                                  }).from_err()
-                              })
-                          });
-}
-
-    Ok(HttpResponse::Ok().finish())
+    data.concat2()
+        .then(|bytes|
+            match bytes {
+                Ok(res) => {
+                    file.expect("Unable to open file").write_all(&res).expect("Unable to save file");
+                    HttpResponse::Ok().finish()
+                },
+                Err(reason) => HttpResponse::InternalServerError().body(format!("{}", reason))
+            }
+        )
 }
 
 #[derive(Deserialize)]
@@ -99,8 +155,9 @@ fn main() -> std::io::Result<()> {
             .service(web::resource("/{bucket_name}/{file_name}").to(get_file))
             .service(web::resource("/api/bucket/{name}/create").to(bucket_create))
             .service(web::resource("/api/bucket/{bucket_name}/{file_name}/upload").route(
-                web::post().to(bucket_upload)
+                web::put().to_async(bucket_upload)
             ))
+            .service(web::resource("/api/bucket/{name}/verify").to(bucket_verify))
     })
         .bind("0.0.0.0:8081").unwrap()
         .start();
